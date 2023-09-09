448e613fcea7b6dd4182c98829a8d5a2a1791561
ifejinity <mrlonzanida08@gmail.com>
2023-09-09 19:20:49 +0800

    9/9/23

--- resources/views/template/login.blade.php
+++ resources/views/template/login.blade.php
@@ -1,17 +1,17 @@
 @extends('layout.layout')
 
 @section('body')
-    <form action="{{ route('login.process') }}" method="POST" class="w-full max-w-[600px] h-fit bg-white rounded-lg shadow-lg md:p-8 p-5 flex flex-col gap-3 mx-[5%] my-auto">
+    <form action="{{ route('login.process') }}" method="POST" class="w-full max-w-[600px] h-fit bg-white rounded-lg shadow-lg md:p-8 p-5 flex flex-col mx-[5%] my-auto">
         @csrf
         <h1 class="text-[30px] font-[700] text-blue-500 mb-3">Sign in</h1>
         <input type="text" name="email" placeholder="Email" class="input bg-gray-200">
         @error('email')
-            <p class="text-red-500 text-[14px] badge badge-error text-white">{{ $message }}</p>
+            <span class="text-red-500 text-[14px] text-red-500 mt-1">{{ $message }}</span>
         @enderror
-        <input type="password" name="password" placeholder="Password" class="input bg-gray-200">
+        <input type="password" name="password" placeholder="Password" class="input bg-gray-200 mt-3">
         @error('password')
-            <p class="text-red-500 text-[14px] badge badge-error text-white">{{ $message }}</p>
+            <span class="text-red-500 text-[14px] text-red-500 mt-1">{{ $message }}</span>
         @enderror
-        <button type="submit" class="btn w-full max-w-[200px] self-end bg-blue-500 hover:bg-blue-400 text-white">Sign in</button>
+        <button type="submit" class="btn w-full max-w-[200px] self-end bg-blue-500 hover:bg-blue-400 text-white mt-3">Sign in</button>
     </form>
 @endsection
